159e7bdb68979380999258d7a883717deff8aad7
ArendtTh <arendt@Mathematik.Uni-Marburg.DE>
2015-04-08 11:57:27 +0200

    initial commit refactoring project

---
+++ de.unimarburg.swt.fopra.state.refactorings/src/de/unimarburg/swt/fopra/state/refactorings/renamestate/RefactoringController.java
@@ -0,0 +1,185 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: RefactoringController.javajet,v 1.2 2012/10/16 21:03:02 tarendt Exp $
+ */
+package de.unimarburg.swt.fopra.state.refactorings.renamestate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.refactor.refactoring.core.Refactoring;
+import org.eclipse.emf.refactor.refactoring.interfaces.IController;
+import org.eclipse.emf.refactor.refactoring.interfaces.IDataManagement;
+import org.eclipse.emf.refactor.refactoring.runtime.ltk.LtkEmfRefactoringProcessorAdapter;
+import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+import org.eclipse.ltk.core.refactoring.participants.RefactoringProcessor;
+
+public final class RefactoringController implements IController{
+
+	/**
+	 * Refactoring supported by the controller.
+	 * @generated
+	 */
+	private Refactoring parent;
+	
+	/**
+	 * DataManagement object of the model refactoring.
+	 * @generated
+	 */
+	private RefactoringDataManagement dataManagement = 
+									new RefactoringDataManagement();
+	
+	/**
+	 * Invocation context of the model refactoring.
+	 * @generated
+	 */	
+	private List<EObject> selection = new ArrayList<EObject>();
+	
+	/**
+	 * Ltk RefactoringProcessor of the model refactoring.
+	 * @generated
+	 */
+	private InternalRefactoringProcessor refactoringProcessor = null;
+	
+	/**
+	 * Gets the Refactoring supported by the controller.
+	 * @return Refactoring supported by the controller.
+	 * @see org.eclipse.emf.refactor.refactoring.interfaces.IController#getParent()
+	 * @generated
+	 */
+	@Override
+	public Refactoring getParent() {
+		return this.parent;
+	}
+	
+	/**
+	 * Sets the Refactoring supported by the controller.
+	 * @param emfRefactoring Refactoring supported by the controller.
+	 * @see org.eclipse.emf.refactor.refactoring.interfaces.IController#
+	 * setParent(org.eclipse.emf.refactor.refactoring.core.Refactoring)
+	 * @generated
+	 */
+	@Override
+	public void setParent(Refactoring emfRefactoring) {
+		this.parent = emfRefactoring;
+	}
+	
+	/**
+	 * Returns the DataManagement object of the model refactoring.
+	 * @return DataManagement object of the model refactoring.
+	 * @see org.eclipse.emf.refactor.refactoring.interfaces.IController#
+	 * getDataManagementObject()
+	 * @generated
+	 */
+	@Override
+	public IDataManagement getDataManagementObject() {
+		return this.dataManagement;
+	}
+
+	/**
+	 * Returns the ltk RefactoringProcessor of the model refactoring.
+	 * @return Ltk RefactoringProcessor of the model refactoring.
+	 * @see org.eclipse.emf.refactor.refactoring.interfaces.IController#
+	 * getLtkRefactoringProcessor()
+	 * @generated
+	 */
+	@Override
+	public RefactoringProcessor getLtkRefactoringProcessor() {
+		return this.refactoringProcessor;
+	}
+	
+	/**
+	 * Sets the selected EObject (invocation context of the model refactoring).
+	 * @param selection Invocation context of the model refactoring.
+	 * @see org.eclipse.emf.refactor.refactoring.interfaces.IController#
+	 * setSelection(java.util.List)
+	 * @generated
+	 */
+	@Override
+	public void setSelection(List<EObject> selection) {
+		this.selection = selection;
+		this.refactoringProcessor = 
+				new InternalRefactoringProcessor(this.selection);
+	}	
+	
+	/**
+	 * Returns a Runnable object that executes the model refactoring.
+	 * @return Runnable object that executes the model refactoring.
+	 * @generated
+	 */
+	private Runnable applyRefactoring() {
+		return new Runnable() {				
+			/**
+			 * @see java.lang.Runnable#run()
+			 * @generated
+			 */
+			@Override
+			public void run() {
+				org.eclipse.uml2.uml.State selectedEObject = 
+					(org.eclipse.uml2.uml.State) dataManagement.
+							getInPortByName(dataManagement.SELECTEDEOBJECT).getValue();
+				String newname =
+					(String) dataManagement.getInPortByName("newname").getValue();
+				selectedEObject.setName(newname);
+			}
+		};
+	}
+
+	/**
+	 * Internal class for providing an instance of a LTK RefactoringProcessor 
+	 * used for EMF model refactorings.	 
+	 * @generated
+	 */
+	public final class InternalRefactoringProcessor extends 
+									LtkEmfRefactoringProcessorAdapter {
+
+		/**
+		 * Constructor using the invocation context of the model refactoring.
+		 * @param selection Invocation context of the model refactoring.
+		 * @generated
+		 */
+		private InternalRefactoringProcessor(List<EObject> selection){
+				super(getParent(), selection, applyRefactoring());				
+		}
+			
+		/**
+		 * @see org.eclipse.ltk.core.refactoring.participants.RefactoringProcessor#
+	 	 * checkInitialConditions(org.eclipse.core.runtime.IProgressMonitor)
+		 * @generated
+		 */	
+		@Override
+		public RefactoringStatus checkInitialConditions(){
+				RefactoringStatus result = new RefactoringStatus();
+				// there is no initial check to do
+				return result;
+		}
+		
+		/**
+		 * @see org.eclipse.ltk.core.refactoring.participants.RefactoringProcessor#
+	     * checkFinalConditions(org.eclipse.core.runtime.IProgressMonitor, 
+	     * org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext)
+		 * @generated
+		 */	
+		@Override
+		public RefactoringStatus checkFinalConditions(){
+				RefactoringStatus result = new RefactoringStatus();
+				org.eclipse.uml2.uml.State selectedEObject = 
+					(org.eclipse.uml2.uml.State) dataManagement.
+							getInPortByName(dataManagement.SELECTEDEOBJECT).getValue();
+				String newname =
+					(String) dataManagement.getInPortByName("newname").getValue();
+				// first final check
+				if (newname.equals("unspecified")) {
+					String msg = "Please insert a new name!!!";
+					result.addFatalError(msg);
+				}
+				// TODO further checks follow
+				return result;
+		}
+		
+	}
+
+}
